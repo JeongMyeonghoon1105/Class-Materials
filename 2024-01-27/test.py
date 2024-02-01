9b5746a0433dbd592a4518af1c8fdd2a4c00288e
JeongMyeonghoon1105 <wjdaudgns1105@naver.com>
2024-02-01 20:29:01 +0900

    Add Column

--- 2024-01-27/test.py
+++ 2024-01-27/test.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from datetime import timedelta as td
 
-df = pd.read_csv("C:/Users/Coding Lab/Desktop/Data-Analysis-Class/2024-01-27/data.csv")
+# df = pd.read_csv("C:/Users/Coding Lab/Desktop/Data-Analysis-Class/2024-01-27/data.csv")
+df = pd.read_csv('/Users/jeongmyeonghun/Desktop/학원 수업/2024-01-27/data.csv')
 a = pd.DataFrame([], columns = df.columns)
 b = pd.DataFrame([], columns = df.columns)
 c = pd.DataFrame([], columns = df.columns)
@@ -12,19 +13,19 @@ for i in range(len(df.index)):
     if 'KR' in df['jkl'][i] or df['PH'][i][0] == 'M':
         b.loc[len(b.index)] = df.loc[i]
     if 'AR' in df['jkl'][i]:
-        c.loc[len(c.index)]                              = df.loc[i]
+        c.loc[len(c.index)] = df.loc[i]
 
-# lst = []
+lst = []
 for i in range(len(a.index)):
     diff = pd.to_datetime(df['ghi'][i]) - pd.to_datetime(df['def'][i])
-    print(diff.days)
-# print(lst)
-# a['test'] = lst
+    lst.append(diff.days * 24 + diff.seconds // 3600)
+print(lst)
+a['test'] = lst
 
 
 # print(df.columns)
 # print()
-# print(a)
+print(a)
 # print()
 # print(b)
 # print()
