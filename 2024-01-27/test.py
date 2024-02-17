7fa926e8d60588f548c0fbd12f66a1bec8b41955
codename-ev <wjdaudgns1105@naver.com>
2024-02-17 20:35:31 +0900

    2024-02-17

--- 2024-01-27/test.py
+++ 2024-01-27/test.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from datetime import timedelta as td
 
-# df = pd.read_csv("C:/Users/Coding Lab/Desktop/Data-Analysis-Class/2024-01-27/data.csv")
-df = pd.read_csv('/Users/jeongmyeonghun/Desktop/학원 수업/2024-01-27/data.csv')
+
+df = pd.read_csv("C:/Users/Coding Lab/Desktop/Data-Analysis-Class/2024-01-27/data.csv")
+# df = pd.read_csv('/Users/jeongmyeonghun/Desktop/학원 수업/2024-01-27/data.csv')
 a = pd.DataFrame([], columns = df.columns)
 b = pd.DataFrame([], columns = df.columns)
 c = pd.DataFrame([], columns = df.columns)
@@ -15,24 +16,65 @@ for i in range(len(df.index)):
     if 'AR' in df['jkl'][i]:
         c.loc[len(c.index)] = df.loc[i]
 
+a_lst = []
 lst = []
 for i in range(len(a.index)):
-    diff = pd.to_datetime(df['ghi'][i]) - pd.to_datetime(df['def'][i])
-    lst.append(diff.days * 24 + diff.seconds // 3600)
-print(lst)
+    diff = pd.to_datetime(a['ghi'][i]) - pd.to_datetime(a['def'][i])
+    a_lst.append([diff.days * 24 + diff.seconds // 3600, (diff.seconds % 3600) // 60])
+    lst.append(str(a_lst[i][0]) + ':' + str(a_lst[i][1]))
 a['test'] = lst
 
+h_sum = 0
+m_sum = 0
+for i in range(len(a_lst)):
+    h_sum += a_lst[i][0]
+    m_sum += a_lst[i][1]
+a.loc[len(a.index)] = ['' for i in range(len(a.columns)-1)] + [str(h_sum // len(a_lst)) + ':' + str(m_sum // len(a_lst))]
 
-# print(df.columns)
-# print()
+
+
+
+b_lst = []
+lst = []
+for i in range(len(b.index)):
+    diff = pd.to_datetime(b['ghi'][i]) - pd.to_datetime(b['def'][i])
+    b_lst.append([diff.days * 24 + diff.seconds // 3600, (diff.seconds % 3600) // 60])
+    lst.append(str(b_lst[i][0]) + ':' + str(b_lst[i][1]))
+b['test'] = lst
+
+h_sum = 0
+m_sum = 0
+for i in range(len(b_lst)):
+    h_sum += b_lst[i][0]
+    m_sum += b_lst[i][1]
+b.loc[len(b.index)] = ['' for i in range(len(b.columns)-1)] + [str(h_sum // len(b_lst)) + ':' + str(m_sum // len(b_lst))]
+
+
+
+
+c_lst = []
+lst = []
+for i in range(len(c.index)):
+    diff = pd.to_datetime(c['ghi'][i]) - pd.to_datetime(c['def'][i])
+    c_lst.append([diff.days * 24 + diff.seconds // 3600, (diff.seconds % 3600) // 60])
+    lst.append(str(c_lst[i][0]) + ':' + str(c_lst[i][1]))
+c['test'] = lst
+
+h_sum = 0
+m_sum = 0
+for i in range(len(c_lst)):
+    h_sum += c_lst[i][0]
+    m_sum += c_lst[i][1]
+c.loc[len(c.index)] = ['' for i in range(len(c.columns)-1)] + [str(h_sum // len(c_lst)) + ':' + str(m_sum // len(c_lst))]
+
 print(a)
-# print()
-# print(b)
-# print()
-# print(c)
-# print()
+print()
+print(b)
+print()
+print(c)
+print()
 
 
 a.to_csv('a.csv', index=False)
-# b.to_csv('b.csv', index=False)
-# c.to_csv('c.csv', index=False)
+b.to_csv('b.csv', index=False)
+c.to_csv('c.csv', index=False)
