dee579b8b19814093e30811726b44e81c195e48f
codename-ev <wjdaudgns1105@naver.com>
2024-03-02 21:08:17 +0900

    2024-03-02

--- 2024-03-02/error_search.py
+++ 2024-03-02/error_search.py
@@ -1,37 +1,34 @@
 import pandas as pd
 
 
-df = pd.read_csv('/Users/jeongmyeonghun/Desktop/학원 수업/2024-03-02/data.csv')
+df = pd.read_csv("C:/Users/Coding Lab/Desktop/Data-Analysis-Class/2024-03-02/data.csv")
+# df = pd.read_csv('/Users/jeongmyeonghun/Desktop/학원 수업/2024-03-02/data.csv')
 
 
 error = '서울'
+data = []
 
 for i in range(len(df.index)):
-  text = ''
-  index = 0
   prev = 0
+  index = 0
+  stack = []
 
-  while index <= len(df['0'][i]):
-    condition_set = [False, False, False, False]
+  for j in range(len(df['1'][i])):
+    if df['1'][i][j] == '(':
+      stack.append('(')
+    elif df['1'][i][j] == ':':
+      if stack:
+        if stack[-1] == '(' or stack[-1] == ':' and stack[-2] == '(':
+          stack.append(':')
+    elif df['1'][i][j] == ')':
+      if stack:
+        if stack[-1] == '(':
+          stack.pop()
+        elif stack[-1] == ':':
+          index = j
+          if error not in df['1'][i][prev:index+1]:
+            data.append([df['0'][i], df['1'][i][prev:index+1]])
+          prev = index+1
 
-    while False in condition_set:
-      print(condition_set)
-      print(df['0'][i][index])
-      if df['0'][i][index] == '(':
-        condition_set[0] = True
-      if condition_set[0] and df['0'][i][index] == ':':
-        condition_set[1] = True
-      if condition_set[0] and condition_set[1] and df['0'][i][index] == ':':
-        condition_set[2] = True
-      if condition_set[0] and condition_set[1] and condition_set[2] and df['0'][i][index] == ')':
-        condition_set[3] = True
-      index += 1
-    
-    if error not in df['0'][i][prev:index]:
-      text += df['0'][i][prev:index]
-    
-    prev = index
-
-  df['0'][i] = text
-
-print(df)
+result = pd.DataFrame(data)
+result.to_csv('result.csv', index=False)
--- 2024-03-02/time_graph.py
+++ 2024-03-02/time_graph.py
@@ -4,8 +4,8 @@ from datetime import datetime
 
 
 # csv 파일을 바탕으로 데이터프레임 생성 & 컬럼명 설정
-# df = pd.read_csv("C:/Users/Coding Lab/Desktop/Data-Analysis-Class/2024-03-02/time_data.csv")
-df = pd.read_csv('/Users/jeongmyeonghun/Desktop/학원 수업/2024-03-02/time_data.csv')
+df = pd.read_csv("C:/Users/Coding Lab/Desktop/Data-Analysis-Class/2024-03-02/time_data.csv")
+# df = pd.read_csv('/Users/jeongmyeonghun/Desktop/학원 수업/2024-03-02/time_data.csv')
 df.columns = ['Start', 'End']
 
 
@@ -36,6 +36,12 @@ plt.barh(y_labels, width, left=left, color='skyblue', edgecolor='black', alpha=0
 plt.gca().xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%Y.%m.%d %H:%M'))
 plt.gca().xaxis.set_major_locator(plt.matplotlib.dates.HourLocator(interval=6))
 plt.xticks(rotation=45)
+
+
+# 각 플롯의 시작 시간과 끝 시간을 그래프에 표기
+for i, (start, end) in enumerate(zip(df['Start'], df['End'])):
+    plt.text(start, i, start.strftime('%Y.%m.%d %H:%M'), fontsize=5, va='center', ha='right')
+    plt.text(end, i, end.strftime('%Y.%m.%d %H:%M'), fontsize=5, va='center', ha='left')
 
 
 # x축, y축 라벨 및 그래프 제목 설정
